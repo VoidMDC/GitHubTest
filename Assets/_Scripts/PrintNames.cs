25e54377a97fc30b1ba6168d02b76d7dfdbc3b7b
rayMillares <rmill013@fiu.edu>
2017-10-14 17:35:15 -0400

    Ray Millares Added

---
+++ Assets/_Scripts/PrintNames.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrintNames : MonoBehaviour {
+
+	private Text _namesTextBox;
+
+	// Please add your full name to the array under this comment. Remember to have a comma before your name and put your name in double quotes. 
+	private string [] names = {"Ray Millares"};
+
+	// Use this for initialization
+	void Start () {
+
+		_namesTextBox = GameObject.Find ("Names").GetComponent<Text>();
+		_namesTextBox.text = "";
+
+		foreach (string name in names)
+		{
+			_namesTextBox.text += name + ", ";
+		}
+
+	}
+}
