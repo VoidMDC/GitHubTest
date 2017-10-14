9c401669f74135a57c4f5278486bbc507c08588c
AlexanderVillarreal <villarrealaw@gmail.com>
2017-10-14 17:46:02 -0400

    AlexanderVillarreal

--- Assets/_Scripts/PrintNames.cs
+++ Assets/_Scripts/PrintNames.cs
@@ -8,7 +8,7 @@ public class PrintNames : MonoBehaviour {
 	private Text _namesTextBox;
 
 	// Please add your full name to the array under this comment. Remember to have a comma before your name and put your name in double quotes. 
-	private string [] names = {"Ray Millares"};
+	private string [] names = {"Ray Millares", "Alexander Villarreal"};
 
 	// Use this for initialization
 	void Start () {
