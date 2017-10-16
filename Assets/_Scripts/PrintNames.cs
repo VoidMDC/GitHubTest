bd8700ae90f8d5064aafb8259eca26237103f53d
William.Tang001@mymdc.net <William.Tang001@mymdc.net>
2017-10-16 09:57:15 -0400

    William Tang added

--- Assets/_Scripts/PrintNames.cs
+++ Assets/_Scripts/PrintNames.cs
@@ -8,7 +8,7 @@ public class PrintNames : MonoBehaviour {
 	private Text _namesTextBox;
 
 	// Please add your full name to the array under this comment. Remember to have a comma before your name and put your name in double quotes. 
-	private string [] names = {"Ray Millares", "Alexander Villarreal", "Gabriel Cabrera"};
+	private string [] names = {"Ray Millares", "Alexander Villarreal", "Gabriel Cabrera", "William Tang"};
 
 	// Use this for initialization
 	void Start () {
