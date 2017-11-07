f8e8743728fc276ec16e567eea886de1731e829d
JadeFoX7 <jadefox7@outlook.com>
2017-11-06 22:01:41 -0500

    Juan C. Gutierrez

--- Assets/_Scripts/PrintNames.cs
+++ Assets/_Scripts/PrintNames.cs
@@ -8,7 +8,7 @@ public class PrintNames : MonoBehaviour {
 	private Text _namesTextBox;
 
 	// Please add your full name to the array under this comment. Remember to have a comma before your name and put your name in double quotes. 
-	private string [] names = {"Ray Millares", "Alexander Villarreal", "Gabriel Cabrera", "William Tang", "Austin Acosta", "Alexander Artigas", "Matthew Monteverde", "Alexander Deluzuriaga"};
+	private string [] names = {"Ray Millares", "Alexander Villarreal", "Gabriel Cabrera", "William Tang", "Austin Acosta", "Alexander Artigas", "Matthew Monteverde", "Alexander Deluzuriaga, Juan C.Gutierrez"};
 
 	// Use this for initialization
 	void Start () {
